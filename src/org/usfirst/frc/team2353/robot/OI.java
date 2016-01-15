becdd0308c1ffff65410ad3372a29add1b136207
vchadha16 <vchadha16@pembrokehill.org>
2016-01-15 14:43:37 -0600

    Cleaned Up OI Class
    Removed waste and finalized OI Class

--- src/org/usfirst/frc/team2353/robot/OI.java
+++ src/org/usfirst/frc/team2353/robot/OI.java
@@ -5,16 +5,14 @@ import edu.wpi.first.wpilibj.AnalogTriggerOutput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
 import org.usfirst.frc.team2353.robot.commands.ExampleCommand;
-import java.util.*;
 
 public class OI {
 	
 	private Joystick xboxController;
 	
 	private Button AButton;
-	private Button BButton;
+	private Button YButton;
 	
 	private AnalogTrigger analog1;
 	private AnalogTrigger analog2;
@@ -28,7 +26,7 @@ public class OI {
 		xboxController = new Joystick(0);
 		
 		AButton = new JoystickButton(xboxController, 0);
-		BButton = new JoystickButton(xboxController, 1);
+		YButton = new JoystickButton(xboxController, 3);
 		
 		analog1 = new AnalogTrigger(1);
 		analog2 = new AnalogTrigger(2);
@@ -36,24 +34,18 @@ public class OI {
 	
 	public void main()
 	{
-		//AButton.whileHeld(Command goes here);
-		//BButton.whileHeld(Command goes here);
+		//AButton.whileHeld(new elvDown());
+		//YButton.whileHeld(new elvUp());
 		
 		leftTred = xboxController.getRawAxis(1); //left analog stick Y Axis
 		rightTred = xboxController.getRawAxis(4); //right analog stick Y Axis
 		
 		if(leftTred > -.2 && leftTred < .2)
-		{
 			leftTred = 0;
-		}else{
-			//Command Goes here?
-		}
 		if(rightTred > -.2 && rightTred < .2)
-		{
 			rightTred = 0;
-		}else{
-			//Command Goes here?
-		}
+
+		new ExampleCommand();
 	}
 }
 
