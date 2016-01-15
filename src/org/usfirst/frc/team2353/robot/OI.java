70c7330048c708d0386061f4ef7b4bf04ed6b7c4
vchadha16 <vchadha16@pembrokehill.org>
2016-01-14 20:47:15 -0600

    Refurbished OI Class
    Possibly finished most of the starting code

--- src/org/usfirst/frc/team2353/robot/OI.java
+++ src/org/usfirst/frc/team2353/robot/OI.java
@@ -8,39 +8,8 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 import org.usfirst.frc.team2353.robot.commands.ExampleCommand;
 import java.util.*;
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
 
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-	//Test
 	
 	private Joystick xboxController;
 	
@@ -49,6 +18,10 @@ public class OI {
 	
 	private AnalogTrigger analog1;
 	private AnalogTrigger analog2;
+	
+	//Directions
+	private double leftTred;
+	private double rightTred;
 	
 	public void init()
 	{
@@ -59,6 +32,28 @@ public class OI {
 		
 		analog1 = new AnalogTrigger(1);
 		analog2 = new AnalogTrigger(2);
+	}
+	
+	public void main()
+	{
+		//AButton.whileHeld(Command goes here);
+		//BButton.whileHeld(Command goes here);
+		
+		leftTred = xboxController.getRawAxis(1); //left analog stick Y Axis
+		rightTred = xboxController.getRawAxis(4); //right analog stick Y Axis
+		
+		if(leftTred > -.2 && leftTred < .2)
+		{
+			leftTred = 0;
+		}else{
+			//Command Goes here?
+		}
+		if(rightTred > -.2 && rightTred < .2)
+		{
+			rightTred = 0;
+		}else{
+			//Command Goes here?
+		}
 	}
 }
 
